269797ee1f08e1dc017f8853efedcabe69253c24
Iqra Siddiqui <59932139+IqraSiddiqui@users.noreply.github.com>
2023-05-06 20:27:39 +0500

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar.jsx"
+import Hero from "./Hero.jsx"
+import Contributors from "./Contributors.jsx"
+import Test from "./Test.jsx"
+import Home from "./Home.jsx"
+import Result from "./Result.jsx"
+
+export {
+    Navbar,
+    Hero,
+    Test,
+    Contributors,
+    Home,
+    Result,
+}
